d264fff19b64928d0e771db19d7dde6f108ad95a
Malvss <120613731+Malvss@users.noreply.github.com>
2023-05-01 21:42:26 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Tugas_AD_baru
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripComboBox2 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripComboBox3 = new System.Windows.Forms.ToolStripComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1,
+            this.toolStripComboBox2,
+            this.toolStripComboBox3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 33);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 33);
+            this.toolStripComboBox1.Click += new System.EventHandler(this.toolStripComboBox1_Click);
+            // 
+            // toolStripComboBox2
+            // 
+            this.toolStripComboBox2.Name = "toolStripComboBox2";
+            this.toolStripComboBox2.Size = new System.Drawing.Size(121, 33);
+            this.toolStripComboBox2.Click += new System.EventHandler(this.toolStripComboBox2_Click);
+            // 
+            // toolStripComboBox3
+            // 
+            this.toolStripComboBox3.Name = "toolStripComboBox3";
+            this.toolStripComboBox3.Size = new System.Drawing.Size(121, 33);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(457, 18);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(328, 404);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox2;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Tugas_AD_baru
+{
+    public partial class Form1 : Form
+    {
+       
+ 
+public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connetionstring = null;
+            MySqlConnection cnn; //connection ini untuk conect in visual studio dan mysql
+            MySqlCommand cmm; // commend untuk query 
+            MySqlDataAdapter adp; // untuk baca query nya 
+            DataTable dt;
+            connetionstring = "server=localhost;database=premier_league;uid=root;pwd=''";
+            cnn = new MySqlConnection(connetionstring);
+            cmm = new MySqlCommand("select m.manager_id as ID,m.manager_name as Name,concat(if (m.working=0,'Available',if(m.manager_id=t.assmanager_id,'Asisten Manager','Manager')),' ',if(m.working=1,t.team_name,'')) as Status from manager m left join team t on m.manager_id=t.assmanager_id or m.manager_id=t.manager_id where m.delete=0 order by ID asc", cnn);
+            adp = new MySqlDataAdapter(cmm);
+            dt = new DataTable();
+
+            try
+
+            {
+                dt = new DataTable();
+                connetionstring = "server=localhost;database=premier_league;uid=root;pwd=''";
+                //cnn = new MySqlConnection(connetionstring);
+                cmm = new MySqlCommand("select team_id,team_name from team t", cnn);
+                adp = new MySqlDataAdapter(cmm);
+                cnn.Open();
+                adp.Fill(dt);
+                toolStripComboBox1.ComboBox.DataSource = dt;
+                toolStripComboBox1.ComboBox.DisplayMember = "team_name";
+                toolStripComboBox1.ComboBox.ValueMember = "team_id";
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection !");
+            }
+            cnn.Close();
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+            string connetionstring = null;
+            MySqlConnection cnn; //connection ini untuk conect in visual studio dan mysql
+            MySqlCommand cmm; // commend untuk query 
+            MySqlDataAdapter adp; // untuk baca query nya 
+            DataTable dt;
+            try
+
+            {
+
+                dt = new DataTable();
+                connetionstring = "server=localhost;database=premier_league;uid=root;pwd=''";
+                cnn = new MySqlConnection(connetionstring);
+                cmm = new MySqlCommand("select player_id,player_name from team t,player p where p.team_id=t.team_id and t.team_id=('" + toolStripComboBox1.ComboBox.SelectedValue + "')", cnn);
+                adp = new MySqlDataAdapter(cmm);
+                cnn.Open();
+                adp.Fill(dt);
+                toolStripComboBox2.ComboBox.DataSource = dt;
+                toolStripComboBox2.ComboBox.DisplayMember = "player_name";
+                toolStripComboBox2.ComboBox.ValueMember = "player_id";
+                cnn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection !");
+            }
+        }
+
+        private void toolStripComboBox2_Click(object sender, EventArgs e)
+        {
+            string connetionstring = null;
+            MySqlConnection cnn; //connection ini untuk conect in visual studio dan mysql
+            MySqlCommand cmm; // commend untuk query 
+            MySqlDataAdapter adp; // untuk baca query nya 
+            DataTable dt;
+            try
+
+            {
+
+                dt = new DataTable();
+                connetionstring = "server=localhost;database=premier_league;uid=root;pwd=''";
+                cnn = new MySqlConnection(connetionstring);
+                cmm = new MySqlCommand("select p.player_name,t.team_name,n.nation,p.playing_pos,sum(if (d.type='CY',1,0)) as 'cy',sum(if(d.type='CR',1,0)) as'cr',sum(if(d.type='GO',1,0)) as 'go', sum(if(d.type='PM',1,0)) as 'pm'from player p,team t,dmatch d,nationality n where p.team_id=t.team_id and d.player_id=p.player_id and p.nationality_id=n.nationality_id and d.team_id=t.team_id and p.player_id=('" + toolStripComboBox2.ComboBox.SelectedValue + "');", cnn);
+                adp = new MySqlDataAdapter(cmm);
+                cnn.Open();
+                adp.Fill(dt);
+                dataGridView1.DataSource = dt;
+                cnn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection !");
+            }
+        }
+    }
+}
